4b6a04446746c2a7cc78a96f4ace533b0e66ec69
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-04-27 17:58:34 +0100

    Persist score changes in mongodb

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,21 +1,30 @@
-import { MongoClient } from "mongodb";
+import { CommentType } from "@/models/types";
+import { MongoClient, ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// POST /api/orders
+// POST /api/comments
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const comment = req.body;
-
   const MONGODB_URI = process.env.MONGODB_URI!;
 
 	const mongoClient = await MongoClient.connect(MONGODB_URI);
   const db = mongoClient.db("interactive-comments-section");
 
-  const ordersCollection = db.collection('comments');
-  const result = await ordersCollection.insertOne(comment);
+  const commentsCollection = db.collection('comments');
+
+  if (req.method === 'POST') {
+    const comment = req.body;
+    const result = await commentsCollection.insertOne(comment);
+    console.log(result);
+  } else if (req.method === 'PUT') {
+    const updatedComment: CommentType = req.body;
+    const result = await commentsCollection.replaceOne({ _id: { $eq: new ObjectId(updatedComment.id) } }, updatedComment);
+    console.log(result);
+    res.status(201).json({ message: 'Comment updated successfully!'});
+  } else if (req.method === 'DELETE') {
+
+  }
 
   mongoClient.close();
-
-  res.status(201).json({ message: 'Order registered successfully!'});
 };
 
 export default handler;
